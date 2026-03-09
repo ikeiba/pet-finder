b78e68ea7d8ef8f3c9ba63ba958e6d9544a7be10
ikeiba <ibarrola.iker@opendeusto.es>
2026-03-09 22:08:36 +0100

    imbalance analysis ready to execute

--- preprocessing/initial_analysis/initial_analysis.py
+++ preprocessing/initial_analysis/initial_analysis.py
@@ -6,11 +6,11 @@ import seaborn as sns
 TRAIN_PATH = "data/train/train.csv"
 TEST_PATH = "data/test/test.csv"
 
-#* We load the main csv for training and testing
+# We load the main csv for training and testing
 data_train = pd.read_csv(filepath_or_buffer=TRAIN_PATH)
 data_test = pd.read_csv(filepath_or_buffer=TEST_PATH)
 
-#* Check the class distribution for the target variable in the train set
+# Check the class distribution for the target variable in the train set
 
 target_distribution_train = data_train["AdoptionSpeed"].value_counts()
 
@@ -22,11 +22,10 @@ print(data_train.dtypes)
 
 print(f"Number of training samples: {data_train.shape}")
 
-# Directo: le pasas la columna y él cuenta los samples por categoría
+#? Directo: le pasas la columna y él cuenta los samples por categoría
 sns.countplot(x='AdoptionSpeed', data=data_train)
 
 plt.title('AdoptionSpeed distribution')
 plt.xlabel('Adoption Speed')
 plt.ylabel('Number of Pets')
 plt.savefig('AdoptionSpeed_distribution.png') 
-
